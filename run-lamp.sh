69f86874b6af59135ba41113736357ac61a99a43
Amarin Boonkirt <amarin-boonkirt@users.noreply.github.com>
2018-09-19 11:50:14 +0700

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -28,6 +28,58 @@ fi
 # enable php short tags:
 /bin/sed -i "s/short_open_tag\ \=\ Off/short_open_tag\ \=\ On/g" /etc/php/7.1/apache2/php.ini
 
+# set new db credential
+/etc/init.d/mysql stop
+mysql -u root -e "update user set password=PASSWORD('') where User='root';update user set plugin='' where User='root';update user set authentication_string='' where User='root';flush privileges;"
+
+/etc/init.d/mysql start
+
+mysql -u root -e "CREATE DATABASE homestead CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';"
+mysql -u root -e "CREATE USER 'homestead'@'localhost' IDENTIFIED BY 'secret';GRANT ALL PRIVILEGES ON * . * TO 'homestead'@'localhost';FLUSH PRIVILEGES;"
+mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
+
+# custom apache config
+
+cat << EOB > /etc/apache2/sites-available/000-default.conf
+
+<VirtualHost *:80>
+    # The ServerName directive sets the request scheme, hostname and port that
+    # the server uses to identify itself. This is used when creating
+    # redirection URLs. In the context of virtual hosts, the ServerName
+    # specifies what hostname must appear in the request's Host: header to
+    # match this virtual host. For the default virtual host (this file) this
+    # value is not decisive as it is used as a last resort host regardless.
+    # However, you must set it for any further virtual host explicitly.
+    #ServerName www.example.com
+
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html
+
+    # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+    # error, crit, alert, emerg.
+    # It is also possible to configure the loglevel for particular
+    # modules, e.g.
+    #LogLevel info ssl:warn
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+    
+    <IfModule mod_headers.c>    
+        <filesMatch "\.(js|css)$">
+            Header set Cache-Control "no-cache, no-store, must-revalidate, private"
+        </filesMatch>    
+    </IfModule>
+
+    # For most configuration files from conf-available/, which are
+    # enabled or disabled at a global level, it is possible to
+    # include a line for only one particular virtual host. For example the
+    # following line enables the CGI configuration for this host only
+    # after it has been globally disabled with "a2disconf".
+    #Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+EOB
+
 # stdout server info:
 if [ ! $LOG_STDOUT ]; then
 cat << EOB
