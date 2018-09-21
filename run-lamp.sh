a34493f687db64e28f1aa29b7890cf8b1af07309
Amarin Boonkirt <amarin-boonkirt@users.noreply.github.com>
2018-09-21 14:04:09 +0700

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -31,12 +31,11 @@ fi
 # set new db credential
 /etc/init.d/mysql stop
 mysqld_safe --skip-grant-tables &
-mysql -u root -e "use mysql;update user set password=PASSWORD('') where User='root';update user set plugin='' where User='root';update user set authentication_string='' where User='root';flush privileges;"
-/etc/init.d/mysql start
-
+mysql -u root -e "use mysql;update user set password=PASSWORD('secret') where User='root';update user set plugin='' where User='root';update user set authentication_string='' where User='root';flush privileges;"
 mysql -u root -e "CREATE DATABASE homestead CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';"
 mysql -u root -e "CREATE USER 'homestead'@'localhost' IDENTIFIED BY 'secret';GRANT ALL PRIVILEGES ON * . * TO 'homestead'@'localhost';FLUSH PRIVILEGES;"
 mysql -u root -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
+/etc/init.d/mysql start
 
 # custom apache config
 
