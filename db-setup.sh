37b56461d891d9a1d1672ccfdea49b812ad920b8
Amarin Boonkirt <amarin-boonkirt@users.noreply.github.com>
2018-10-03 19:41:53 +0700

    Create db-setup.sh

---
+++ db-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mysql -u root -psecret -e "CREATE DATABASE homestead CHARACTER SET = 'utf8' COLLATE = 'utf8_unicode_ci';"
+mysql -u root -psecret -e "CREATE USER 'homestead'@'localhost' IDENTIFIED BY 'secret';GRANT ALL PRIVILEGES ON * . * TO 'homestead'@'localhost';FLUSH PRIVILEGES;"
+mysql -u root -psecret -e "CREATE USER 'admin'@'localhost' IDENTIFIED BY 'admin';GRANT ALL PRIVILEGES ON * . * TO 'admin'@'localhost';FLUSH PRIVILEGES;"
